3be1e0bdf28fdc6969cae471e30b734bb2e27fe8
Kartik Kapoor <kartikkapoor3390@gmail.com>
2024-07-27 23:05:42 -0600

    Remove Property old COlumn

--- internal/property/model.go
+++ internal/property/model.go
@@ -1,42 +1,12 @@
 package property
 
 import (
-	"backend/internal/model"
 	"time"
 )
 
-var Table model.Table = model.Table{
-	Name: "property",
-	Fields: []model.Column{
-		{
-			Name:     "id",
-			DataType: "VARCHAR(255)",
-			Options:  "PRIMARY KEY",
-		},
-		{
-			Name:     "company_id",
-			DataType: "VARCHAR(255)",
-			Options:  "NOT NULL UNIQUE",
-		},
-		{
-			Name:     "address",
-			DataType: "VARCHAR(255)",
-			Options:  "NOT NULL",
-		},
-		{
-			Name:     "unit_ids",
-			DataType: "VARCHAR(255)[]",
-		},
-		{
-			Name:     "team_ids",
-			DataType: "VARCHAR(255)[]",
-		},
-	},
-}
-
 type Property struct {
-	ID        string    `db:"id"`
-	CompanyID string    `db:"company_id"`
+	ID        string    `db:"id" db_opts:"primary_key"`
+	CompanyID string    `db:"company_id" db_opts:"not_null"`
 	Address   string    `db:"address"`
 	UnitIDs   []string  `db:"unit_ids"`
 	TeamIDs   []string  `db:"team_ids"`
